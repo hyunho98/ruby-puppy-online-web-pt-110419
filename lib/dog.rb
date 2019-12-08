bcdd7a481642a6a69f5f52b3c855f0e5659fff54
Alex Lee <hyunho98@gmail.com>
2019-12-08 19:58:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+  @@all = []
+
+  def initialize
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each{|i| puts i}
+  end
+
+  def save
+    @@all << self
+  end
+end
